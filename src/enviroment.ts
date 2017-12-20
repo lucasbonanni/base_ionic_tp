884118f21f3b114c15718eb1832b54ae5027990d
danag <le_bonanni@yahoo.com.ar>
2017-12-19 22:33:16 -0300

    Chage the enviroment for firebase configuration constant

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,11 +1,10 @@
-export const environment = {
-    production: false,
-    firebase: {
+export const firebaseConfig = {
         apiKey: '<your-key>',
         authDomain: '<your-project-authdomain>',
         databaseURL: '<your-database-URL>',
         projectId: '<your-project-id>',
         storageBucket: '<your-storage-bucket>',
         messagingSenderId: '<your-messaging-sender-id>'
-    }
 };
+
+
