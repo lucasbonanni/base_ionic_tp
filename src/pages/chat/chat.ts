a493af3f4a4990133b53e87857b4f5b3a3879694
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-06-01 19:58:13 -0300

    Se cambian algunos estilos, se agega una opción para selecionar cada sala de chat

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content, Platform } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { LoginRegister } from '../login-register/login-register';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
@@ -30,13 +30,17 @@ export class ChatPage implements OnInit {
   public messages :Observable<Message[]>;
   public messagesRef : AngularFireList<Message>;
   public newMessage: Message;
+  public messagesT:Message[];
   user: User;
   displayName: string = '';
+  salaValue:string = '';
+  @ViewChild('content') content: Content
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private auth: AuthServiceProvider,
-    private db: AngularFireDatabase) {
+    private db: AngularFireDatabase,
+    private platform: Platform) {
     this.newMessage = new Message();
   }
   ngOnInit(): void {
@@ -44,11 +48,23 @@ export class ChatPage implements OnInit {
     this.displayName = this.user.displayName;
     this.messagesRef = this.db.list<Message>('List/4a');
     this.messages = this.messagesRef.valueChanges();
-    console.log(this.messagesRef);
-  }
-  ionViewDidLoad() {
+    // this.messagesRef.valueChanges().subscribe(next =>{
+    //   this.messagesT = next;
+    //   this.salaValue = '4A';
+    //   this.content.resize();
+    //   this.content.scrollToBottom();
+    // }, () =>{}, () =>{
+    //   this.content.scrollToBottom();
+    // });
+    // console.log(this.messagesRef);
   }
 
+  ionViewDidLoad(){
+    // this.platform.ready().then(value => {
+    //   this.content.resize();
+    //   this.content.scrollToBottom();
+    // });
+  }
   public logout() {
     this.auth.signOut().then(() => {
       this.navCtrl.setRoot(LoginRegister)
@@ -61,20 +77,32 @@ export class ChatPage implements OnInit {
       const date = new Date();
       this.newMessage.date = date.toString();
       this.newMessage.sender = this.displayName;
+      this.newMessage.profilePhoto = this.user.photoURL;
       this.messagesRef.push(this.newMessage);
     }
     this.newMessage.messageText = '';
+    this.content.scrollToBottom();
   }
 
 
   public chageToClass4b(){
     this.messagesRef = this.db.list<Message>('List/4b');
+    this.salaValue = '4B';
     this.messages = this.messagesRef.valueChanges();
   }
 
   public chageToClass4a(){
     this.messagesRef = this.db.list<Message>('List/4a');
+    this.salaValue = '4A';
     this.messages = this.messagesRef.valueChanges();
+    
+  }
+
+  public toBottomFunction(){
+    this.content.resize();
+    this.content.getContentDimensions();
+    this.content.scrollToBottom(0);
+    console.log('test');
   }
 
 }
